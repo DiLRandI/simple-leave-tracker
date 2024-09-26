df6c062abf5e32cbc4d047f576d9e8c36d19a4b2
Deleema Randimal <deleemarf@gmail.com>
2024-09-26 20:45:48 +0530

    add initial gorm support.

---
+++ internal/storage/db/postgres.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+	"log/slog"
+
+	"simple-leave-tracker/internal/storage"
+	"simple-leave-tracker/internal/storage/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type mySQL struct {
+	db *gorm.DB
+}
+
+func New(dsn string, log *slog.Logger) (storage.Storage, error) {
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		return nil, fmt.Errorf("error when connecting to mysql, %w", err)
+	}
+
+	if err := db.AutoMigrate(model.GetModels()...); err != nil {
+		return nil, fmt.Errorf("error when migrating modes, %w", err)
+	}
+
+	return &mySQL{
+		db: db,
+	}, nil
+}
---
+++ internal/storage/model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+
+	FirstName string
+	LastName  string
+	Email     string
+
+	Login Login
+}
